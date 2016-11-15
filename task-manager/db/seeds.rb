886f95d7d6b7b742d6df359a455bb26d309509ce
Domitille Chenut <domitille.de.percin@gmail.com>
2016-11-15 19:23:26 +0100

    controller index show

--- task-manager/db/seeds.rb
+++ task-manager/db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#
+  Task.create(name:"menage")
+  Task.create(name:"repassage")
+  Task.create(name:"courses")
+  Task.create(name:"taf")
